f45c90d9bb648a8c84cc88ea7c29da99b0b78cbf
rudy750 <rudy750@gmail.com>
2019-07-15 23:23:17 -0400

    got a card working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-container class="application theme--light"> 
+  <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+
+  <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+
+
+  <!-- <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+  >
+    <v-toolbar color="grey darken-1" dark>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-menu :nudge-width="100">
+        <template v-slot:activator="{ on }">
+          <v-toolbar-title v-on="on">
+            <span>All</span>
+            <v-icon dark>arrow_drop_down</v-icon>
+          </v-toolbar-title>
+        </template>
+
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item"
+            @click=""
+          >
+            <v-list-tile-title v-text="item"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card> -->
+</v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { VFlex, VCard, VImg, VBtn, VCardTitle, VCardActions, VContainer } from 'vuetify/lib'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    VFlex, VCard, VImg, VBtn, VCardTitle, VCardActions, VContainer
+  },
+  data () {
+    return {
+      items: [
+        'All', 'Family', 'Friends', 'Coworkers'
+      ],
+      card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
